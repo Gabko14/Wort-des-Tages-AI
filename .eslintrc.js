29afdac7aaad497b16b5ee30679980baffdff123
Qazi10Ayan <Qazi10Ayan@users.noreply.github.com>
2025-12-03 16:06:37 +0100

    #18 Coderichtlinien doku & anpassen configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,86 @@
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'import'],
   rules: {
     'prettier/prettier': 'warn',
+
+    // Import-Sortierung
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-native'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
   },
   ignorePatterns: ['/dist/*'],
   env: {
     node: true,
   },
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
   overrides: [
+    // TypeScript-Dateien: Naming Conventions
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'warn',
+          // Variablen: camelCase oder UPPER_CASE oder PascalCase (für Komponenten)
+          {
+            selector: 'variable',
+            format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+          },
+          // Funktionen: camelCase oder PascalCase (für Komponenten)
+          {
+            selector: 'function',
+            format: ['camelCase', 'PascalCase'],
+          },
+          // Typen & Interfaces: PascalCase
+          {
+            selector: 'typeLike',
+            format: ['PascalCase'],
+          },
+        ],
+      },
+    },
+    // Test-Dateien
     {
       files: [
         '**/__tests__/**/*.[jt]s?(x)',
